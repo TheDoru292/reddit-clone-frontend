f62725829066c2d54d43b653235c21215c08888d
thedoru <96445851+TheDoru292@users.noreply.github.com>
2023-08-22 15:10:37 +0200

    chore: add breakpoints for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ export default {
     extend: {
       screens: {
         hmd: { raw: '(max-height: 700px)' },
-        hsm: { raw: '(max-height: 320px)' }
+        hsm: { raw: '(max-height: 320px)' },
+        one: { raw: '(min-width: 1190px)' },
+        two: { raw: '(min-width: 1060px)' },
+        three: { raw: '(min-width: 970px)' }
       }
     }
   },
